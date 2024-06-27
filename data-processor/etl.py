393151ce1a219f12f5e8059583353d697cba69f0
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-06-27 06:44:33 +0000

    merged extract and transform to etl module

--- data-processor/etl.py
+++ data-processor/etl.py
@@ -1,11 +1,18 @@
 import numpy 
 import configparser
 
+'''
+
+
+'''
+
+
 class Extract:
 
     def __init__(self):
         self.configparser_object = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
         self.configparser_object.read("configuration/csv_file_path_configuration.txt")
+        
 
     def retrieve_data_for_active_cases_of_renting_out_of_flat(self) -> numpy.ndarray:
 
@@ -57,4 +64,39 @@ class Extract:
         data = numpy.loadtxt(file_name, skiprows=1, dtype=[('quarter', 'U7'), ('flat_type', 'U9'), ('no_of_approvals', 'i4')], delimiter=",")
 
         return data
+
+
+'''
+
+
+'''
+
+
+class Transform:
+
+    def __init__(self):
+        pass
+
+    def transform_median_rent_by_town_and_flat_type_dataset(self) -> numpy.ndarray:
+        pass
     
+
+    def transform_renting_out_of_flat_approvals_by_flat_type_quarterly_dataset(self) -> numpy.ndarray:
+        pass
+
+    def transform_price_range_of_hdb_flats_offered_dataset(self) -> numpy.ndarray:
+        pass
+
+
+    def transform_number_of_sold_and_rented_hdb_residential_units_dataset(self) -> numpy.ndarray:
+        pass
+
+    
+    def transform_median_resale_prices_for_registered_applications_by_town_and_flat_type_dataset(self) -> numpy.ndarray:
+        pass
+
+    def transform_demand_for_rental_and_sold_flats_dataset(self) -> numpy.ndarray:
+        pass
+
+    def transform_active_cases_of_renting_out_of_flat_dataset(self) -> numpy.ndarray:
+        pass
