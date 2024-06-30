063e0a79f9b0fb55e9db68c20b0f3f2e36002c78
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-06-30 13:40:45 +0000

    added methods to describe the dataset and create statistical analysis using five number summary in the dataset

--- data_processor/describe.py
+++ data_processor/describe.py
@@ -1,17 +1,87 @@
 import numpy
-
+import math
 
 class Describe:
     
-    def perform_dataset_analysis(self, array: numpy.ndarray):
+
+    def determine_numerical_data_type(self, data_type: numpy.dtype) -> bool:
+
+        if data_type not in [numpy.dtypes.BoolDType, numpy.dtypes.ByteDType, numpy.dtypes.BytesDType, numpy.dtypes.Complex128DType, numpy.dtypes.Complex64DType, numpy.dtypes.DateTime64DType, numpy.dtypes.ObjectDType, numpy.dtypes.StrDType, numpy.dtypes.VoidDType, numpy.dtypes.UByteDType, numpy.dtypes.TimeDelta64DType]:
+            return True
+        else:
+            return False
+
+
+    def perform_dataset_analysis(self, title: str , dataset: numpy.ndarray) -> None:
+
+        print("***{0}***".format(title))
+
+        print("")
+
+        print("There are {0} rows and {1} columns in this dataset".format(len(dataset), len(dataset.dtype.names)))
+
+        print("")
+
+        print("The names of the columns are: ")
+
+        for column in dataset.dtype.names:
+            
+            numeric_data_status: bool = self.determine_numerical_data_type(dataset[column].dtype)
+            if numeric_data_status == False:
+                if False in numpy.char.isnumeric(dataset[column]):
+                    numeric_data_status = False
+                else:
+                    numeric_data_status = True
+
+            print("- {0} {1} isnumeric: {2} ".format(column, type(dataset[column].item(0)), numeric_data_status))
+
         
+        print("")
 
-        #Generate a data dictionary
+        for column in dataset.dtype.names:
+            print("There are {0} unique values in {1} column".format(len(numpy.unique(dataset[column])), column))
 
-        pass
 
-    def perform_statistical_analysis(self, array: numpy.ndarray):
-        pass
+
+    def perform_statistical_analysis(self, dataset_title: str , dataset: numpy.ndarray, columns_to_exclude: list = []) -> None:
+
+        print("***Five number summary for {0} dataset**".format(dataset_title))
+
+        # if there are any numerical data type then perform the numeric data type 
+
+        # minimum value
+        # Q1
+        # median value
+        # Q3
+        # maximum value 
+
+
+        for column in dataset.dtype.names:
+
+            if column not in columns_to_exclude:
+
+                determine_numeric_data_type: bool = self.determine_numerical_data_type(dataset[column].dtype)
+
+                if determine_numeric_data_type == True:
+
+                    first_quartile_array_position: int = math.ceil((len(dataset[column]) + 1) * 0.25)
+                    mid_quartile_array_position: int = math.ceil((len(dataset[column]) + 1) * 0.50)
+                    third_quartile_array_position: int = math.ceil((len(dataset[column]) + 1) * 0.75)
+
+                    print("Column: {0}".format(column))
+                    print("")
+
+                    print("Minimum value: {0}".format(numpy.min(dataset[column])))
+                    print("Q1: {0}".format(dataset[column][first_quartile_array_position]))
+                    print("Median: {0}".format(dataset[column][mid_quartile_array_position]))
+                    print("Q3: {0}".format(dataset[column][third_quartile_array_position]))
+                    print("Maximum value: {0}".format(numpy.max(dataset[column])))
+
+                    print("")
+                    print("")
+
+
+
 
     def generate_data_dictionary(self):
         pass        
