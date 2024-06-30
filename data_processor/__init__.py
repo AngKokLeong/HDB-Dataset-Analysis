bda0297c777efc8d76a8da986e6b64856608f5c4
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-06-30 13:39:37 +0000

    added describe into __init__.py and initialize the Describe object

--- data_processor/__init__.py
+++ data_processor/__init__.py
@@ -1,5 +1,6 @@
 import configparser
 import data_processor.etl
+import data_processor.describe
 
 file_path: str = __path__[0]
 
@@ -11,4 +12,9 @@ config.set("DEFAULT", "project_file_path", file_path)
 
 extract = data_processor.etl.Extract(config)
 transform = data_processor.etl.Transform()
+describe = data_processor.describe.Describe()
 
+
+#data_profiling
+    #define structure discovery
+    #define content discovery
